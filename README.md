07a79aff8477f469f7e62b204fc3ee5e6fc07bda
Noah Heck <noah@noahheck.com>
2020-06-25 18:26:51 -0600

    Add install script and initial instructions

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # office-forge-install
+
+This script will install and configure a suitable environment for running the Office Forge application (or other LAMP/Laravel application).
+
+It is designed and tested to run effectively on a standard Digital Ocean Ubuntu 20.04 installation. For other environments/operating systems/hosts/etc, your mileage may vary.
+
+Aside from taking the opinionated actions of
+
+- disabling the default 000-default and default-ssl Apache vhosts
+- creating an officeforge database and mysql user
+
+this script should be suitable to execute on an existing environment, though this hasn't been tested.
+
+More details will follow as the Office Forge installation process is refined.
---
+++ of-install.sh
@@ -0,0 +1,142 @@
+#! /usr/bin/env bash
+# Office Forge installation script
+#
+# (c) Pillar Falls Software, LLC
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Please run this as root"
+    exit 1
+fi
+
+echo "What is the ServerName for this installation?"
+read server_name
+
+echo "Updating system packages..."
+apt update && apt upgrade -y
+
+echo "Installing apache2..."
+apt install -y apache2 zip certbot python3-certbot-apache
+
+echo "Enabling mod_ssl and mod_rewrite..."
+a2enmod ssl && a2enmod rewrite
+
+echo "Disabling default sites"
+a2dissite 000-default
+a2dissite default-ssl
+
+echo "Installing mysql-server..."
+apt install -y mysql-server
+
+echo "Running mysql_secure_installation with defaults..."
+mysql_secure_installation --use-default
+
+echo "Creating officeforge database..."
+mysql -e "CREATE DATABASE officeforge CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
+
+echo "What is the database user's password?"
+echo "Make sure to include upper and lowercase characters, numbers, and special characters"
+read db_password
+
+echo "Creating officeforge database user..."
+mysql -e "CREATE USER 'officeforge'@'localhost' IDENTIFIED BY '$db_password'"
+mysql -e "GRANT ALL PRIVILEGES ON officeforge.* TO 'officeforge'@'localhost' WITH GRANT OPTION"
+
+echo "Installing PHP..."
+apt install -y php libapache2-mod-php php-mysql php-bcmath php-json php-mbstring php-tokenizer php-xml
+
+
+
+echo "Installing composer..."
+
+
+EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "Error installing composer"
+    echo "Unable to complete setup"
+    echo "Exiting"
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    exit 1
+fi
+
+php composer-setup.php --quiet
+RESULT=$?
+rm composer-setup.php
+
+mv composer.phar /usr/local/bin/composer
+
+
+
+
+
+
+echo "Creating site directory..."
+mkdir /var/www/officeforge/
+
+echo "Cloning Office Forge repository..."
+git clone https://github.com/noahheck/office-forge.git /var/www/officeforge/
+
+
+echo "Creating Office Forge vhost..."
+siteconf="/etc/apache2/sites-available/officeforge.conf"
+touch $siteconf
+
+echo "<VirtualHost *:80>" >> $siteconf
+echo "    ServerName $server_name" >> $siteconf
+echo "    ServerAdmin  webmaster@localhost" >> $siteconf
+echo "    DocumentRoot /var/www/officeforge/public" >> $siteconf
+echo "    ErrorLog ${APACHE_LOG_DIR}/error.log" >> $siteconf
+echo "    CustomLog ${APACHE_LOG_DIR}/access.log combined" >> $siteconf
+echo "    <Directory /var/www/officeforge/public>" >> $siteconf
+echo "        Options FollowSymLinks" >> $siteconf
+echo "        AllowOverride All" >> $siteconf
+echo "        Require all granted" >> $siteconf
+echo "    </Directory>" >> $siteconf
+echo "</VirtualHost>" >> $siteconf
+
+echo "Enabling Office Forge vhost..."
+a2ensite officeforge
+
+echo "Restarting apache2 service..."
+apache2ctl restart
+
+echo "Running certbot..."
+echo "Please make sure to redirect all traffic to the ssl secured port..."
+certbot --apache
+
+echo "Changing directory to Office Forge root..."
+cd /var/www/officeforge/
+
+echo "Installing composer dependencies..."
+composer install
+
+echo "Copying .env.example to .env..."
+cp .env.example .env
+
+echo "Running artisan key:generate..."
+./artisan key:generate
+
+echo "Changing ownership of project files and direcories..."
+chown -R www-data:www-data /var/www/officeforge
+
+
+
+echo "Installation complete!"
+echo ""
+echo "The database user's password is:"
+echo "    $db_password"
+echo ""
+
+echo "Please set the database password in the .env file"
+echo "and make sure you run"
+echo ""
+echo "    ./artisan migrate"
